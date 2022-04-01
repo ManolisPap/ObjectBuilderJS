fd78a9dac3467e2a1585161335cec30177acd16a
Manolis Papadospyridakis <manolispapadospiridakis@gmail.com>
2022-04-01 21:49:05 +0300

    update the readme

---
+++ README.md
@@ -0,0 +1,52 @@
+# Object Builder
+
+---
+
+This package will help you to create an object based on a string description.
+
+Example:
+
+```js
+// Example 1
+const user = objectBuilder('name', {}, 'Bob');
+objectBuilder('surname', user, 'Smith');
+objectBuilder('address[0].zipCode', user, '1234');
+objectBuilder('address[1].zipCode', user, '5678');
+
+// user would look like
+/*
+{
+  name: 'Bob',
+  surname: 'Smith',
+  address: [ { zipCode: '1234' }, { zipCode: '5678' } ]
+}
+*/
+```
+
+**Todo's:**
+
+So far, it works only for object as top level element.
+
+Do not use array as top level element:
+
+```js
+let tmp = objectBuilder('addresses[0].zipCode[0]', [], 70200);
+objectBuilder('addresses[0].zipCode[1]', tmp, 70201);
+objectBuilder('addresses[1].zipCode[0]', tmp, 70400);
+console.dir(tmp, { depth: null });
+
+/*
+Result: Is not the expected
+[
+  {
+    addresses: [ { zipCode: [ 70200 ] } ]
+  },
+  {
+    addresses: [ { zipCode: [ 70201 ] } ]
+  },
+  {
+    addresses: [ { zipCode: [ 70400 ] } ]
+  }
+]
+*/
+```
--- tests/object.test.ts
+++ tests/object.test.ts
@@ -107,8 +107,8 @@ describe('Object Builder, Arrays', () => {
       numbers: [123, 345],
     };
 
-    let tmp = objectBuilder('numbers[0]', {}, 123);
-    tmp = objectBuilder('numbers[1]', tmp, 345);
+    const tmp = objectBuilder('numbers[0]', {}, 123);
+    objectBuilder('numbers[1]', tmp, 345);
 
     expect(tmp).toStrictEqual(obj);
   });
@@ -121,9 +121,9 @@ describe('Object Builder, Arrays', () => {
       },
     };
 
-    let tmp = objectBuilder('address.numbers[0]', {}, 123);
-    tmp = objectBuilder('address.numbers[1]', tmp, 345);
-    tmp = objectBuilder('address.address', tmp, 'Kalamaki');
+    const tmp = objectBuilder('address.numbers[0]', {}, 123);
+    objectBuilder('address.numbers[1]', tmp, 345);
+    objectBuilder('address.address', tmp, 'Kalamaki');
 
     expect(tmp).toStrictEqual(obj);
   });
@@ -137,9 +137,8 @@ describe('Object Builder, Arrays', () => {
       },
     };
 
-    let tmp = objectBuilder('user.company.addresses[0].telephones[0]', {}, 123);
-
-    tmp = objectBuilder('user.company.addresses[0].telephones[1]', tmp, 345);
+    const tmp = objectBuilder('user.company.addresses[0].telephones[0]', {}, 123);
+    objectBuilder('user.company.addresses[0].telephones[1]', tmp, 345);
 
     expect(tmp).toStrictEqual(obj);
   });
@@ -179,9 +178,9 @@ describe('Object Builder, Nesting objects with multiple properties', () => {
     };
 
     const tmp = objectBuilder('name', {}, 'Manolis');
-    const result = objectBuilder('surname', tmp, 'Papadospyridakis');
+    objectBuilder('surname', tmp, 'Papadospyridakis');
 
-    expect(result).toStrictEqual(obj);
+    expect(tmp).toStrictEqual(obj);
   });
 
   test('Test D.2', () => {
@@ -193,9 +192,9 @@ describe('Object Builder, Nesting objects with multiple properties', () => {
     };
 
     const tmp = objectBuilder('user.name', {}, 'Manolis');
-    const result = objectBuilder('user.surname', tmp, 'Papadospyridakis');
+    objectBuilder('user.surname', tmp, 'Papadospyridakis');
 
-    expect(result).toStrictEqual(obj);
+    expect(tmp).toStrictEqual(obj);
   });
 
   test('Test D.3', () => {
@@ -236,8 +235,8 @@ describe('Object Builder, Arrays with multiple elements', () => {
       },
     };
     const tmp = objectBuilder('user.company.addresses[0].address', {}, 'address1');
-    const result = objectBuilder('user.company.addresses[1].address', tmp, 'address2');
-    expect(result).toStrictEqual(obj);
+    objectBuilder('user.company.addresses[1].address', tmp, 'address2');
+    expect(tmp).toStrictEqual(obj);
   });
 
   test('Test E.3', () => {
@@ -249,8 +248,8 @@ describe('Object Builder, Arrays with multiple elements', () => {
       },
     };
     const tmp = objectBuilder('user.company.addresses[0].telephones[0].number', {}, 123);
-    const result = objectBuilder('user.company.addresses[0].telephones[1].number', tmp, 345);
-    expect(result).toStrictEqual(obj);
+    objectBuilder('user.company.addresses[0].telephones[1].number', tmp, 345);
+    expect(tmp).toStrictEqual(obj);
   });
 
   test('Test E.4', () => {
@@ -262,8 +261,8 @@ describe('Object Builder, Arrays with multiple elements', () => {
       },
     };
     const tmp = objectBuilder('user.company.addresses[0].telephones[0].numbers[0].1', {}, 123);
-    const result = objectBuilder('user.company.addresses[0].telephones[0].numbers[1].2', tmp, 345);
-    expect(result).toStrictEqual(obj);
+    objectBuilder('user.company.addresses[0].telephones[0].numbers[1].2', tmp, 345);
+    expect(tmp).toStrictEqual(obj);
   });
 
   test('Test E.5', () => {
@@ -279,8 +278,8 @@ describe('Object Builder, Arrays with multiple elements', () => {
       },
     };
     const tmp = objectBuilder('user.company.addresses[0].telephones[0].numbers[0].1', {}, 123);
-    const result = objectBuilder('user.company.addresses[0].telephones[1].numbers[0].1', tmp, 345);
-    expect(result).toStrictEqual(obj);
+    objectBuilder('user.company.addresses[0].telephones[1].numbers[0].1', tmp, 345);
+    expect(tmp).toStrictEqual(obj);
   });
 
   test('Test E.6', () => {
@@ -299,8 +298,8 @@ describe('Object Builder, Arrays with multiple elements', () => {
       },
     };
     const tmp = objectBuilder('user.company.addresses[0].telephones[0].numbers[0].1', {}, 123);
-    const result = objectBuilder('user.company.addresses[1].telephones[0].numbers[0].1', tmp, 345);
-    expect(result).toStrictEqual(obj);
+    objectBuilder('user.company.addresses[1].telephones[0].numbers[0].1', tmp, 345);
+    expect(tmp).toStrictEqual(obj);
   });
 
   test('Test E.7', () => {
@@ -314,11 +313,11 @@ describe('Object Builder, Arrays with multiple elements', () => {
         },
       },
     };
-    let tmp = objectBuilder('user.company.addresses[0].address', {}, 'address1');
-    tmp = objectBuilder('user.company.addresses[0].zipCode', tmp, 123);
-    tmp = objectBuilder('user.company.addresses[1].address', tmp, 'address2');
-    const result = objectBuilder('user.company.addresses[1].zipCode', tmp, 345);
-    expect(result).toStrictEqual(obj);
+    const tmp = objectBuilder('user.company.addresses[0].address', {}, 'address1');
+    objectBuilder('user.company.addresses[0].zipCode', tmp, 123);
+    objectBuilder('user.company.addresses[1].address', tmp, 'address2');
+    objectBuilder('user.company.addresses[1].zipCode', tmp, 345);
+    expect(tmp).toStrictEqual(obj);
   });
 });
 
@@ -337,13 +336,13 @@ describe('Object Builder, Arrays and Objects with multiple elements', () => {
       },
     };
 
-    let tmp = objectBuilder('user.name', {}, 'Manolis');
-    tmp = objectBuilder('user.company.addresses[0].address', tmp, 'Kalamaki1');
-    tmp = objectBuilder('user.company.addresses[1].address', tmp, 'Kalamaki2');
-    tmp = objectBuilder('user.company.vat', tmp, 123);
-    const result = objectBuilder('user.lastname', tmp, 'Papadospyridakis');
+    const tmp = objectBuilder('user.name', {}, 'Manolis');
+    objectBuilder('user.company.addresses[0].address', tmp, 'Kalamaki1');
+    objectBuilder('user.company.addresses[1].address', tmp, 'Kalamaki2');
+    objectBuilder('user.company.vat', tmp, 123);
+    objectBuilder('user.lastname', tmp, 'Papadospyridakis');
 
-    expect(result).toStrictEqual(obj);
+    expect(tmp).toStrictEqual(obj);
   });
 
   test('Test F.2', () => {
@@ -379,26 +378,26 @@ describe('Object Builder, Arrays and Objects with multiple elements', () => {
       },
     };
 
-    let tmp = objectBuilder('user.email', {}, 'manolis123@gmail.com');
-    tmp = objectBuilder('user.password', tmp, '12345678');
-    tmp = objectBuilder('user.name', tmp, 'Manolis');
-    tmp = objectBuilder('user.surname', tmp, 'Papadospyridakis');
-    tmp = objectBuilder('user.role', tmp, 'COURIER_COMPANY');
+    const tmp = objectBuilder('user.email', {}, 'manolis123@gmail.com');
+    objectBuilder('user.password', tmp, '12345678');
+    objectBuilder('user.name', tmp, 'Manolis');
+    objectBuilder('user.surname', tmp, 'Papadospyridakis');
+    objectBuilder('user.role', tmp, 'COURIER_COMPANY');
 
-    tmp = objectBuilder('user.company.companyName', tmp, 'Art Of Jewelry');
-    tmp = objectBuilder('user.company.vat', tmp, '050405368');
-    tmp = objectBuilder('user.company.isIndividual', tmp, false);
+    objectBuilder('user.company.companyName', tmp, 'Art Of Jewelry');
+    objectBuilder('user.company.vat', tmp, '050405368');
+    objectBuilder('user.company.isIndividual', tmp, false);
 
-    tmp = objectBuilder('user.company.addresses[0].address', tmp, 'Kalamaki');
-    tmp = objectBuilder('user.company.addresses[0].zipCode', tmp, '70200');
-    tmp = objectBuilder('user.company.addresses[0].city', tmp, 'Kalamaki');
-    tmp = objectBuilder('user.company.addresses[0].floor', tmp, 1);
-    tmp = objectBuilder('user.company.addresses[0].doorNumber', tmp, '1');
+    objectBuilder('user.company.addresses[0].address', tmp, 'Kalamaki');
+    objectBuilder('user.company.addresses[0].zipCode', tmp, '70200');
+    objectBuilder('user.company.addresses[0].city', tmp, 'Kalamaki');
+    objectBuilder('user.company.addresses[0].floor', tmp, 1);
+    objectBuilder('user.company.addresses[0].doorNumber', tmp, '1');
 
-    tmp = objectBuilder('user.company.addresses[0].telephones[0].number', tmp, '6955546039');
-    const result = objectBuilder('user.company.addresses[0].telephones[1].number', tmp, '2892045475');
+    objectBuilder('user.company.addresses[0].telephones[0].number', tmp, '6955546039');
+    objectBuilder('user.company.addresses[0].telephones[1].number', tmp, '2892045475');
 
-    expect(result).toStrictEqual(obj);
+    expect(tmp).toStrictEqual(obj);
   });
 
   test('Test F.3', () => {
@@ -452,35 +451,35 @@ describe('Object Builder, Arrays and Objects with multiple elements', () => {
       },
     };
 
-    let tmp = objectBuilder('order.courierCompanyId', {}, 1);
-    tmp = objectBuilder('order.pickupAddressId', tmp, 1);
-    tmp = objectBuilder('order.pickupDateFrom', tmp, '31-03-2022');
-    tmp = objectBuilder('order.pickupDateTo', tmp, '01-04-2022');
-    tmp = objectBuilder('order.deliveryDateFrom', tmp, '02-04-2022');
-    tmp = objectBuilder('order.deliveryDateTo', tmp, '03-04-2022');
-    tmp = objectBuilder('order.recipient.name', tmp, 'Kostas');
-    tmp = objectBuilder('order.recipient.surname', tmp, 'Papadospyridakis');
-    tmp = objectBuilder('order.recipient.companyName', tmp, '-');
-    tmp = objectBuilder('order.recipient.vat', tmp, '12341243');
-    tmp = objectBuilder('order.recipient.addresses[0].address', tmp, 'Leykosia');
-    tmp = objectBuilder('order.recipient.addresses[0].zipCode', tmp, '3034');
-    tmp = objectBuilder('order.recipient.addresses[0].city', tmp, 'Leykosia');
-    tmp = objectBuilder('order.recipient.addresses[0].floor', tmp, 3);
-    tmp = objectBuilder('order.recipient.addresses[0].doorNumber', tmp, '4a');
-    tmp = objectBuilder('order.recipient.addresses[0].telephones[0].number', tmp, '6903456934');
-    tmp = objectBuilder('order.recipient.addresses[0].telephones[1].number', tmp, '2828282828');
+    const tmp = objectBuilder('order.courierCompanyId', {}, 1);
+    objectBuilder('order.pickupAddressId', tmp, 1);
+    objectBuilder('order.pickupDateFrom', tmp, '31-03-2022');
+    objectBuilder('order.pickupDateTo', tmp, '01-04-2022');
+    objectBuilder('order.deliveryDateFrom', tmp, '02-04-2022');
+    objectBuilder('order.deliveryDateTo', tmp, '03-04-2022');
+    objectBuilder('order.recipient.name', tmp, 'Kostas');
+    objectBuilder('order.recipient.surname', tmp, 'Papadospyridakis');
+    objectBuilder('order.recipient.companyName', tmp, '-');
+    objectBuilder('order.recipient.vat', tmp, '12341243');
+    objectBuilder('order.recipient.addresses[0].address', tmp, 'Leykosia');
+    objectBuilder('order.recipient.addresses[0].zipCode', tmp, '3034');
+    objectBuilder('order.recipient.addresses[0].city', tmp, 'Leykosia');
+    objectBuilder('order.recipient.addresses[0].floor', tmp, 3);
+    objectBuilder('order.recipient.addresses[0].doorNumber', tmp, '4a');
+    objectBuilder('order.recipient.addresses[0].telephones[0].number', tmp, '6903456934');
+    objectBuilder('order.recipient.addresses[0].telephones[1].number', tmp, '2828282828');
 
-    tmp = objectBuilder('order.packages[0].weight', tmp, 1.5);
-    tmp = objectBuilder('order.packages[0].height', tmp, 10);
-    tmp = objectBuilder('order.packages[0].width', tmp, 10);
-    tmp = objectBuilder('order.packages[0].length', tmp, 10);
-    tmp = objectBuilder('order.packages[0].comments', tmp, 'Hi!');
+    objectBuilder('order.packages[0].weight', tmp, 1.5);
+    objectBuilder('order.packages[0].height', tmp, 10);
+    objectBuilder('order.packages[0].width', tmp, 10);
+    objectBuilder('order.packages[0].length', tmp, 10);
+    objectBuilder('order.packages[0].comments', tmp, 'Hi!');
 
-    tmp = objectBuilder('order.packages[1].weight', tmp, 2);
-    tmp = objectBuilder('order.packages[1].height', tmp, 20);
-    tmp = objectBuilder('order.packages[1].width', tmp, 20);
-    tmp = objectBuilder('order.packages[1].length', tmp, 20);
-    tmp = objectBuilder('order.packages[1].comments', tmp, 'Hi2!');
+    objectBuilder('order.packages[1].weight', tmp, 2);
+    objectBuilder('order.packages[1].height', tmp, 20);
+    objectBuilder('order.packages[1].width', tmp, 20);
+    objectBuilder('order.packages[1].length', tmp, 20);
+    objectBuilder('order.packages[1].comments', tmp, 'Hi2!');
 
     expect(tmp).toStrictEqual(obj);
   });
